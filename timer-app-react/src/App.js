9dcefdc082283382393bd2a1173e6a1b74aa1615
sunnyxtian <134821187+sunnyxtian@users.noreply.github.com>
2024-08-16 21:40:18 -0700

    added react router. commented dev plan for switching between modes.

--- timer-app-react/src/App.js
+++ timer-app-react/src/App.js
@@ -9,6 +9,25 @@ import Timer from './components/Timer.js';
 import NavBar from './components/NavBar';
 
 function App() {
+
+  // DEV PLAN
+
+  // initialize browser router
+
+  // have the current interval and interval number as a state variable
+  // and then pass the callback setter function as a prob to the timer component
+  // this will allow the timer component to increment the current interval number
+
+  // we can use division to figure out if the interval is a break or focus session
+
+  // we need to implement a routing system with react browser router
+  // that routes to the summary page instead of the timer page when current
+  // interval is final one.
+
+  // have the break and focus times as a state variable
+  // pass the setter function to the settings component to let the user edit
+  // break and focus times
+
   return (
     <div className="App">
       <header className="App-header">
