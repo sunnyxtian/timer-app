579148e513f73e704562de15c178d3af067ab16f
sunnyxtian <134821187+sunnyxtian@users.noreply.github.com>
2024-08-18 20:00:28 -0700

    added state variables to app.js

--- timer-app-react/src/App.js
+++ timer-app-react/src/App.js
@@ -5,6 +5,7 @@
  */
 
 import { Routes, Route } from 'react-router-dom';
+import React, { useState } from 'react';
 
 import './App.css';
 import Timer from './components/Timer.js';
@@ -17,25 +18,31 @@ function App() {
 
   // DONE initialize browser router
 
-  // 1. have the current interval and interval number as a state variable
+  // DONE 1. have the current interval and interval number as a state variable
   // and then pass the callback setter function as a prob to the timer component
   // this will allow the timer component to increment the current interval number
+  const [currInterval, setCurrInterval] = useState(1);
+  const [numIntervals, setNumIntervals] = useState(8);
 
-  // 2. we can use division to figure out if the interval is a break or focus session
-
-  // 3. we need to implement a routing system with react browser router
-  // that routes to the summary page instead of the timer page when current
-  // interval is final one.
-
-  // 4. have the break and focus durations as a state variable
+  // DONE 2. have the break and focus durations as a state variable
   // pass the setter function to the settings component to let the user edit
   // break and focus times
+  const [breakDurationMins, setBreakDurationMins] = useState(5);
+  const [focusDurationMins, setFocusDurationMins] = useState(20);
+
+  // 3. Make wrapper functions to pass the consts downwards.
+
+  // 4. we can use division to figure out if the interval is a break or focus session
+
+  // 5. we need to implement a routing system with react browser router
+  // that routes to the summary page instead of the timer page when current
+  // interval is final one.
 
   return (
     <div className="App">
       <NavBar/>
       <Routes>
-        <Route path="" element={<Timer currentInterval="0" totalInterval="3" /*setter function*/
+        <Route path="" element={<Timer currentInterval={currInterval} totalInterval={numIntervals} /*setter function*/
                 /*focusState={focusStateBool}*//>} />
         <Route path="settings" element={<Settings/>} />
       </Routes>
