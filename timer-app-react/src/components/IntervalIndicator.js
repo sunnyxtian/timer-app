9b56f10d910507c36745df5f82c0758e59723795
sunnyxtian <134821187+sunnyxtian@users.noreply.github.com>
2024-09-07 21:54:44 -0700

    edited IntervalIndicator plan

--- timer-app-react/src/components/IntervalIndicator.js
+++ timer-app-react/src/components/IntervalIndicator.js
@@ -11,18 +11,19 @@ export default IntervalIndicator;
 
 /**
  * This componets represents all the focus states of our pomodoro timer. It will display the
- * current interval and the total number of intervals visual in ovals. Each oval will be filled
- * according to the current time in the interval.
+ * current interval and the total number of focus intervals as rounded rectangles.
+ * Each rectangle will be filled according to the current time in the interval.
  *
- * 1.) Get info about the total number of intervals and the current interval from the parent
- * component
+ * 1.) Get info about the total number of focus intervals and the current interval from
+ *     the parent component
  *
- * 2.) Display all the total intervals in ovals and fill them with the color grey
+ * 2.) Display all the total intervals in rectangles and fill them with the color grey
+ *     (grey indicates incomplete)
  *
- * 3.) Color the current interval oval with the color yellow
+ * 3.) As the time progresses in the current interval, fill the rectangle with the color
+ *     yellow from from left to right (YouTube tutorials + a bit of math needed)
+ *     (yellow indicates in progress)
  *
- * 4.) As the time progresses in the current interval, fill the oval with the color yellow from
- *     from left to right (YouTube tutorials + a bit of math needed)
- *
- * 5.) When the current interval is over, color the oval green and move to the next oval
+ * 4.) When the current interval is over, color the oval green and move to the next oval
+ *     (green indicates complete)
  */
